6ddb1ce9b8b121481decac7b9091bddd2659753e
suvambasak <suvambasak2008@gmail.com>
2021-01-16 16:52:27 +0530

    error status

--- processes/9_exe_commands.c
+++ processes/9_exe_commands.c
@@ -1,25 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include<sys/wait.h>
+#include <sys/wait.h>
 
-int main(){
+int main()
+{
 
     int id = fork();
 
-    if (-1==id)
+    if (-1 == id)
     {
         printf("fork failed\n");
         return -1;
     }
 
-    if(0==id){
+    if (0 == id)
+    {
         // Child process.
-        execlp("ping","ping","-c","3","google.com",NULL);
-    }else{
+        int err = execlp("ping", "ping", "-c", "3", "google.com", NULL);
+        // if command is wrong!
+        if (-1 == err)
+        {
+            printf("Not found!\n");
+            return -1;
+        }
+    }
+    else
+    {
         // Main process.
-        wait(NULL);
-        printf("Done! \n");
+
+        // If something went wrong in command exe.
+        int status;
+        wait(&status);
+        if (WIFEXITED(status))
+        {
+            int status_code = WIFEXITED(status);
+            if (0 == status_code)
+            {
+                printf("Done! \n");
+            }
+            else
+            {
+                printf("NOT-Done! Error code : %d \n", status_code);
+            }
+        }
     }
 
     return 0;

52a18bd12327548d00b9455377867f5288b1b57d
suvambasak <suvambasak2008@gmail.com>
2021-01-16 23:42:11 +0530

    thread in c

---
+++ threads/1_intro.c
@@ -0,0 +1,40 @@
+// -pthread
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void *fun()
+{
+    printf("Start Thread!\n");
+    sleep(3);
+    printf("End Thread!\n");
+}
+
+int main()
+{
+    pthread_t t1, t2;
+
+    if (0 != pthread_create(&t1, NULL, &fun, NULL))
+    {
+        printf("Error\n");
+        return -1;
+    }
+    if (0 != pthread_create(&t2, NULL, &fun, NULL))
+    {
+        printf("Error\n");
+        return -1;
+    }
+    if (0 != pthread_join(t1, NULL))
+    {
+        printf("Error\n");
+        return -1;
+    }
+    if (0 != pthread_join(t2, NULL))
+    {
+        printf("Error\n");
+        return -1;
+    }
+
+    return 0;
+}

1d40ed0c70c115a6d6925ce6248c6f27a6c9f0e3
suvambasak <suvambasak2008@gmail.com>
2021-01-17 12:01:26 +0530

    pthread exit to return value

---
+++ threads/10_pthread_exit.c
@@ -0,0 +1,42 @@
+// -pthread
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+
+void *roll()
+{
+    int *value = (int *)malloc(sizeof(int));
+    *value = 1 + (rand() % 6);
+    printf("Thread value : %d\n", *value);
+    // return (void *)value;
+    pthread_exit((void *)value); // works like return.
+}
+
+int main()
+{
+    int *value;
+    srand(time(NULL));
+    pthread_t th;
+
+    if (0 != pthread_create(&th, NULL, &roll, NULL))
+    {
+        printf("Error\n");
+        return -1;
+    }
+
+    // exit(0); // Terminate the process
+    pthread_exit(0); // doesn't terminate the process.
+
+    if (0 != pthread_join(th, (void **)&value))
+    {
+        printf("Error\n");
+        return -1;
+    }
+
+    printf("Value in main : %d \n",*value);
+    free(value);
+
+    return 0;
+}
